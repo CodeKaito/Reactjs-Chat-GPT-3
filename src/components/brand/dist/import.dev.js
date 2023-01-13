3b0d41c225463939833f45d9da8c6e304074b8b5
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-01-13 17:12:34 +0100

    Add files

---
+++ src/components/brand/dist/import.dev.js
@@ -0,0 +1,47 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "google", {
+  enumerable: true,
+  get: function get() {
+    return _google["default"];
+  }
+});
+Object.defineProperty(exports, "slack", {
+  enumerable: true,
+  get: function get() {
+    return _slack["default"];
+  }
+});
+Object.defineProperty(exports, "atlassian", {
+  enumerable: true,
+  get: function get() {
+    return _atlassian["default"];
+  }
+});
+Object.defineProperty(exports, "dropbox", {
+  enumerable: true,
+  get: function get() {
+    return _dropbox["default"];
+  }
+});
+Object.defineProperty(exports, "shopify", {
+  enumerable: true,
+  get: function get() {
+    return _shopify["default"];
+  }
+});
+
+var _google = _interopRequireDefault(require("../../assets/google.png"));
+
+var _slack = _interopRequireDefault(require("../../assets/slack.png"));
+
+var _atlassian = _interopRequireDefault(require("../../assets/atlassian.png"));
+
+var _dropbox = _interopRequireDefault(require("../../assets/dropbox.png"));
+
+var _shopify = _interopRequireDefault(require("../../assets/shopify.png"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ src/containers/dist/index.dev.js
@@ -0,0 +1,55 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Blog", {
+  enumerable: true,
+  get: function get() {
+    return _Blog["default"];
+  }
+});
+Object.defineProperty(exports, "Features", {
+  enumerable: true,
+  get: function get() {
+    return _Features["default"];
+  }
+});
+Object.defineProperty(exports, "Footer", {
+  enumerable: true,
+  get: function get() {
+    return _Footer["default"];
+  }
+});
+Object.defineProperty(exports, "Header", {
+  enumerable: true,
+  get: function get() {
+    return _Header["default"];
+  }
+});
+Object.defineProperty(exports, "Possibility", {
+  enumerable: true,
+  get: function get() {
+    return _Possibility["default"];
+  }
+});
+Object.defineProperty(exports, "WhatGPT3", {
+  enumerable: true,
+  get: function get() {
+    return _WhatGPT["default"];
+  }
+});
+
+var _Blog = _interopRequireDefault(require("./blog/Blog"));
+
+var _Features = _interopRequireDefault(require("./features/Features"));
+
+var _Footer = _interopRequireDefault(require("./footer/Footer"));
+
+var _Header = _interopRequireDefault(require("./header/Header"));
+
+var _Possibility = _interopRequireDefault(require("./possibility/Possibility"));
+
+var _WhatGPT = _interopRequireDefault(require("./whatGPT3/WhatGPT3"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
